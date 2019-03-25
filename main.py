6e43fdafe9e2ba68e44d96eac75d796c7416c512
Leonardo Medeiros <leostayner@gmail.com>
2019-03-25 15:43:46 -0300

    arvore implementada

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from token import *
 from prepro import *
 from parser import *
 
+def readFile(file_name):
+    with open(file_name, 'r') as f:
+        code = ''.join(f.readlines())
+        return code
+       
 def main():
     try:
-        code = input("Digite uma operação: ")
+        code = readFile("input.txt")
         code = PrePro.filter(code)
+        print("Operação: {0}".format(code))
         
         if len(code) > 0:
-                Parser.run(str(code))
-        
+                result = Parser.run(str(code))
+                print(result.Evaluate())
+
     except Exception as err:
         print(err)
 
---
+++ node.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self):
+        self.value      #variant
+        self.children   #list of nodes
+
+    def Evaluate():
+        pass
+
+class BinOp(Node):
+    def __init__(self, value, children):
+        self.value     = value 
+        self.children  = children
+ 
+    def Evaluate(self):
+        if self.value == "/":
+            return self.children[0].Evaluate() / self.children[1].Evaluate()
+
+        elif self.value == "*":
+            return self.children[0].Evaluate() * self.children[1].Evaluate()
+
+        elif self.value == "+":
+            return self.children[0].Evaluate() + self.children[1].Evaluate()
+
+        elif self.value == "-":
+            return self.children[0].Evaluate() - self.children[1].Evaluate()
+        
+
+class UnOp(Node):
+    def __init__(self, value, children):
+        self.value     = value 
+        self.children  = children
+ 
+    def Evaluate(self):
+        if self.value == "+":
+            return  + int(self.children[0].Evaluate())
+
+        return - int(self.children[0].Evaluate())
+
+class IntVal(Node):
+    def __init__(self, value):
+        self.value     = value
+ 
+    def Evaluate(self):
+        return self.value
+
+class NoOp(Node):
+    def __init__(self, value, children):
+        self.value     = value 
+        self.children  = children
+         
+    def Evaluate(self):
+        pass
