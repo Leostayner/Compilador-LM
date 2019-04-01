746c83319270cd973231cbcffff49fe46be68cb7
Leonardo Medeiros <leostayner@gmail.com>
2019-04-01 16:39:42 -0300

    arquivo por entrada padrao do python

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 #Compilador - LM
 #python3
 
+import sys
 from token import *
 from prepro import *
 from parser import *
 
 def readFile(file_name):
     with open(file_name, 'r') as f:
-        return f.readlines()
-       
+        return ''.join(f.readlines())
+      
 def main():
-    code = readFile("input.vbs")    
-    for line in code:
-        try:
-                code = PrePro.filter(line)
-                if len(code) > 0:
-                        print("Operação: {0}".format(code))
-                        result = Parser.run(str(code))
-                        print(result.Evaluate(), "\n")
+    try:
+        code = readFile(sys.argv[1])    
+        code = PrePro.filter(code)
+        if len(code) > 0:
+                print("Operação: {0}".format(code))
+                result = Parser.run(str(code))
+                print(result.Evaluate(), "\n")
 
-        except Exception as err:
-                print(err, "\n")
-        
-main()
+    except Exception as err:
+        print(err, "\n")
+
+main() 
